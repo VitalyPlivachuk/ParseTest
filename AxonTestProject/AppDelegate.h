41040d934745e99c5faa3090f32cca67bfa2b8a9
Vitaly Plivachuk <vitaly.plivachuk@gmail.com>
2018-08-30 10:56:31 +0300

    Initial Commit

---
+++ AxonTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AxonTestProject
+//
+//  Created by Vitaly Plivachuk on 8/30/18.
+//  Copyright © 2018 Vitaly Plivachuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AxonTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AxonTestProject
+//
+//  Created by Vitaly Plivachuk on 8/30/18.
+//  Copyright © 2018 Vitaly Plivachuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
